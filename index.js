248ad6d93e260f2d9aa66c0a315527976093ae3a
Miriam Castillo <castillo.m.mc@gmail.com>
2017-12-12 23:14:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@
      return "You will see great things from Scuber."
    }
 
-   this.prototype.sayHi = function(){
+   BoardMember.prototype.sayHi = function(){
      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
    }
  }
